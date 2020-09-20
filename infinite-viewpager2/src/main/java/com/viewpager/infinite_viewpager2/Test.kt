b951e92efb230a1e785e16811dff550adc21c8ec
watermelon-mann <vahek39@gmail.com>
2020-09-20 20:20:54 +0400

    Create repository

---
+++ infinite-viewpager2/src/main/java/com/viewpager/infinite_viewpager2/Test.kt
@@ -0,0 +1,12 @@
+package com.viewpager.infinite_viewpager2
+
+import android.util.Log
+
+class Test {
+
+    companion object {
+        fun makeTestLog() {
+            Log.d("TestLibrary", "makeTestLog: This Works!!")
+        }
+    }
+}
