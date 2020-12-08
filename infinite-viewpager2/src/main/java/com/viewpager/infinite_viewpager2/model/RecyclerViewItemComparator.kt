64b4c14afcb60e2b03187762ea87099f706330ca
watermelon-mann <vahek39@gmail.com>
2020-12-08 23:28:59 +0400

    alfa version

--- infinite-viewpager2/src/main/java/com/viewpager/infinite_viewpager2/model/RecyclerViewItemComparator.kt
+++
@@ -1,6 +0,0 @@
-package com.viewpager.infinite_viewpager2.model
-
-interface RecyclerViewItemComparator {
-    fun isItemTheSame(other: Any) : Boolean
-    fun isContentTheSame(other: Any) : Boolean
-}
